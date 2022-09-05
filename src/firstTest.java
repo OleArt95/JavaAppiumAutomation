dcc934320bcebc5581a32260e3cb8ea386074e0b
OlegArt95 <Wickodin@gmail.com>
2022-09-05 20:29:54 +0300

    create method waitForElementPresentByXpath refactor wait methods

--- src/firstTest.java
+++ src/firstTest.java
@@ -3,7 +3,11 @@ import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
 
@@ -24,15 +28,62 @@ public class firstTest {
         capabilities.setCapability("app", "/Users/olegartyuhov/Desktop/JavaAppiumAutomation/apks/org.wikipedia.com.apk");
 
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+
+        WebElement skipButton = driver.findElementById("org.wikipedia:id/fragment_onboarding_skip_button");
+        skipButton.click();
     }
 
     @After
     public void tearDown() {
+
         driver.quit();
     }
 
     @Test
     public void firstTest() {
-        System.out.println("first test run");
+        waitForElementByXpathAndClick(
+                "//*[contains(@text,'Search Wikipedia')]",
+                "Cannot find Search Wikipedia input",
+                5
+        );
+
+        waitForElementByXpathAndSendKeys(
+                "//*[contains(@text,'Search Wikipedia')]",
+                "Java",
+                "Cannot find search input",
+                5
+        );
+
+        waitForElementPresentByXpath(
+                "//*[@resource-id='org.wikipedia:id/search_results_list']//*[@text='Object-oriented programming language']",
+                "Cannot find 'Object-oriented programming language' topic searching by Java",
+                15
+        );
+    }
+
+    private WebElement waitForElementPresentByXpath(String xpath, String error_message, long timeoutInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(error_message + "\n");
+        By by = By.xpath(xpath);
+        return wait.until(
+                ExpectedConditions.presenceOfElementLocated(by)
+        );
+    }
+
+    private WebElement waitForElementPresentByXpath(String xpath, String error_message) {
+
+        return waitForElementPresentByXpath(xpath, error_message, 5);
+    }
+
+    private WebElement waitForElementByXpathAndClick(String xpath, String error_message, long timeoutInSeconds) {
+        WebElement element = waitForElementPresentByXpath(xpath, error_message, timeoutInSeconds);
+        element.click();
+        return element;
+    }
+
+    private WebElement waitForElementByXpathAndSendKeys(String xpath, String value, String error_message, long timeoutInSeconds) {
+        WebElement element = waitForElementPresentByXpath(xpath, error_message, timeoutInSeconds);
+        element.sendKeys(value);
+        return element;
     }
 }
